93878ae875602d98e147672ed56718c18c02a7db
Hai Long <nguyenhailong270921@gmail.com>
2022-01-08 12:02:27 +0700

    Complete interface design

---
+++ music.js
@@ -0,0 +1,47 @@
+
+const app = {
+
+    songs: [
+        
+        { name: 'Nevada',
+          singer: 'Vicetone',
+          path: './assets/music/nevada.mp3',
+          image: './assets/img/nevadaVN.webp'
+        },
+        { name: 'SummerTime',
+          singer: 'K-391',
+          path: './assets/music/summertime.mp3',
+          image: './assets/img/summerTime.webp'
+        },
+        { name: 'Monody',
+          singer: 'TheFatRat',
+          path: './assets/music/monody.mp3',
+          image: './assets/img/monody.webp'
+        },
+        { name: 'Reality',
+          singer: 'Lost Frequencies',
+          path: './assets/music/reality.mp3',
+          image: './assets/img/reality.webpp'
+        },
+        { name: 'Nơi Này Có Anh',
+          singer: 'Sơn Tùng-MTP',
+          path: './assets/music/noiNayCoAnh.mp3',
+          image: './assets/img/noiNayCoAnh.webp'
+        },
+        { name: 'Thì Thôi',
+          singer: 'Ready',
+          path: './assets/music/thiThoi.mp3',
+          image: './assets/img/thiThoi.webp'
+        },
+        { name: 'Suýt Nữa Thì',
+          singer: 'Andiez',
+          path: './assets/music/suytNuaThi.mp3',
+          image: './assets/img/suytNuaThi.webp'
+        },
+        { name: 'Chờ Anh Nhé',
+          singer: 'Hoàng Dũng',
+          path: './assets/music/choAnhNhe.mp3',
+          image: './assets/img/choAnhNhe.webp'
+        }
+    ]
+}
